288ef8d912bb348a95dfb8fe63b17aa1a947575e
KGlol <micyounian@gmail.com>
2018-12-11 18:53:24 +0800

    Updates

---
+++ precache-manifest.564179a495469298f393f838866784f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "fb5fd24da2a77ad9b1cd",
+    "url": "/burger-builder/static/js/runtime~main.fb5fd24d.js"
+  },
+  {
+    "revision": "8e0255625633fead79d5",
+    "url": "/burger-builder/static/js/main.8e025562.chunk.js"
+  },
+  {
+    "revision": "fe9dbe58bd5b81e35bd0",
+    "url": "/burger-builder/static/js/1.fe9dbe58.chunk.js"
+  },
+  {
+    "revision": "8e0255625633fead79d5",
+    "url": "/burger-builder/static/css/main.ff42e53a.chunk.css"
+  },
+  {
+    "revision": "24af77dbda7ae77fdf61e0bd58518a9e",
+    "url": "/burger-builder/index.html"
+  }
+];
